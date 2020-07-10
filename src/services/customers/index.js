f2b7b983c30698f9568c26087ba3565d00b4a15a
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-07-10 17:31:46 +0300

    Create index.js

---
+++ src/services/customers/index.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path")
+const {join} = require("path")
+const multer = require("multer")
+const {writeFile} = require("fs-extra")
+const q2m = require("query-to-mongo")
+const customerRoute = express.Router();
+const CustomerSchema = require("./schema");
+const customerModel = require("./schema");
+const ProductModel = require("../products/schema")
+
+customerRoute.get("/", async(req, res, next)=>{
+    const customers = await CustomerSchema.find(req.query)
+    
+    res.send({data:customers, Total: customers.length})
+    
+})
+customerRoute.get("/:id", async(req, res, next)=>{
+    try {
+        const id = req.params.id
+        const customer =await CustomerSchema.findById(id)
+        
+        if(customer){
+          res.send(customer)
+        }else{
+          const error = new Error()
+          error.httpStatusCode = 404;
+          next(error)
+        }
+       } catch (error) {
+         error.httpStatusCode = 404
+         next("While reading customers from DB problem occured") // next is sending the error to the error handler
+       }
+    
+})
+customerRoute.post("/", async(req, res, next)=>{
+    try {
+      
+        const newCustomer = new CustomerSchema(req.body)
+          const{_id} = await newCustomer.save()
+          res.status(201).send("New Customers added with Id: "+_id)
+            
+       
+       
+        }catch (error) {
+        next(error)
+      }
+    
+})
+customerRoute.put("/:id", async(req, res, next)=>{
+    const customer = await CustomerSchema.findByIdAndUpdate(req.params.id, req.body)
+  if(customer){
+    res.status(204).send(customer)
+  }else{
+    const error = new Error(`Customer with id ${req.params.id} not found`);
+    error.httpStatusCode=404
+    next(error)
+  }
+ 
+    
+})
+customerRoute.delete("/:id", async(req, res, next)=>{
+    const customer = await CustomerSchema.findByIdAndDelete(req.params.id);
+  if(customer){
+    res.send(`Deleted customers with id: ${req.params.id}`)
+  }else {
+    const error = new Error(`customer with id ${req.params.id} not found`)
+    error.httpStatusCode = 404
+    next(error)
+  }
+    
+})
+
+
+customerRoute.post("/:id/add-to-cart/:productId", async (req, res, next) => {
+    try {
+      
+      const product = await ProductModel.productReview(req.params.productId)
+      if (product) {
+        const newproduct = { ...product.toObject(), quantity: 1 }
+
+        const isProductThere = await CustomerSchema.findProductInCart(
+          req.params.id,
+          req.params.productId
+        )
+        if (isProductThere) {
+         
+          await CustomerSchema.incrementCartQuantity(
+            req.params.id,
+            req.params.productId,
+            1
+          )
+          res.send("Quantity incremented")
+        } else {
+          // the product is not in the cart
+          await customerModel.addProductToCart(req.params.id, newproduct)
+          res.send("New product added!")
+        }
+      } else {
+        const error = new Error()
+        error.httpStatusCode = 404
+        next(error)
+      }
+    } catch (error) {
+      next(error)
+    }
+  })
+  
+  customerRoute.delete("/:id/remove-from-cart/:productId", async (req, res, next) => {
+    try {
+      await CustomerSchema.removeProductFromCart(req.params.id, req.params.productId)
+      res.send("Ok")
+    } catch (error) {
+      next(error)
+    }
+  })
+  
+  customerRoute.get("/:id/calculate-cart-total", async (req, res, next) => {
+    try {
+      const total = await CustomerSchema.calculateCartTotal(req.params.id)
+      res.send({ total })
+    } catch (error) {
+      next(error)
+    }
+  })
+module.exports=customerRoute;
